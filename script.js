3aa38eb0da0c0c9b4e98eec862f5868833f9a247
Sriramachandra <sriramachandra2387.k@gmail.com>
2022-02-15 13:38:46 +0530

    look and feel

--- script.js
+++ script.js
@@ -32,10 +32,10 @@ class Particle {
     constructor() {
         this.x = mouse.x;
         this.y = mouse.y;
-        this.size = Math.random() * 5;
-        this.speedX = Math.random() *3 - 1.5;
+        this.size = Math.random() * 25;
+        this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
-        this.radius = 40;
+        this.radius = 10;
     }
 
     update() {
@@ -43,30 +43,38 @@ class Particle {
         this.y += this.speedY;
 
         //Control the mouse animation here
-        if (this.speedX < 2 && this.radius > 0.9 || 
-            this.speedY < 2 && this.radius > 0.9) {
-            this.radius--;
-        }
+        // if (this.speedX < 2 && this.radius > 0.9 || 
+        //     this.speedY < 2 && this.radius > 0.9) {
+        //     this.radius--;
+        // }
+
+        if(this.size > 0.1) this.size -= 1;
     }
 
     draw() {
-        ctx.fillStyle = 'white';
-        ctx.strokeStyle = 'red';
+        // ctx.fillStyle = 'firebrick';
+        ctx.strokeStyle = 'white';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0 , Math.PI * 2);
         ctx.stroke();
-        ctx.fill();
+        // ctx.fill();
     }
 }
 //========================================================================================
 function animate() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    // ctx.fillStyle= 'rgb(255,255,255,255,0.02)';
-    // ctx.fillRect(0,0,canvas.width, canvas.height);
+    // ctx.clearRect(0,0,canvas.width, canvas.height);
+    // ctx.fillStyle='black';
+    ctx.fillStyle= 'rgba(0,0,0,0.1)';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
 
     for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
         particlesArray[i].draw();
+
+        if(particlesArray[i].size <=4) {
+            particlesArray.splice(i,1);
+            i--;
+        }
         // for (let j = 0; j < particlesArray.length; j++) {
         //     const dx = particlesArray[j].x - particlesArray[i].x;
         //     const dy = particlesArray[j].y - particlesArray[i].y;
@@ -85,7 +93,7 @@ function animate() {
 }
 //=======================================================================================
 function handleParticles() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 10; i++) {
         particlesArray.push(new Particle());
     }
     // console.log(particlesArray);
